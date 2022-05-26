63252663bd655daaf2d660cfd7d2a599e8dc7c69
Marc <m.spiess@digitalux.ch>
2022-05-26 05:50:58 +0200

    Update docs

--- theme.config.js
+++ theme.config.js
@@ -1,13 +1,10 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
+  github: 'https://github.com/vuetronic/framework',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
-      </span>
+      <span className="mr-2 font-extrabold hidden md:inline">vuetronic</span>
     </>
   ),
   head: (

5a3c5dfa8aac9e6c8c780dcff10497ed92bed646
Marc <m.spiess@digitalux.ch>
2022-05-26 05:57:03 +0200

    Update docs

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,7 @@
 export default {
   github: 'https://github.com/vuetronic/framework',
   docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  titleSuffix: ' – Vuetronic',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">vuetronic</span>
@@ -13,15 +13,15 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Build Electron Application with Vue.js and Typescriptr" />
+      <meta name="og:description" content="Build Electron Application with Vue.js and Typescript" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="twitter:url" content="https://vuetronic.org" />
+      <meta name="og:title" content="Vuetronic - Build Electron Application with Vue.js and Typescript" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="apple-mobile-web-app-title" content="vuetronic" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -59,6 +59,6 @@ export default {
   nextLinks: true,
   footer: true,
   footerEditLink: 'Edit this page on GitHub',
-  footerText: <>MIT {new Date().getFullYear()} © Nextra.</>,
+  footerText: <>{new Date().getFullYear()} © vuetronic.</>,
   unstable_faviconGlyph: '👋',
 }
